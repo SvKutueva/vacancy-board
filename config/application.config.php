29cb93d085d33187f4f39386bd8d0776aedf517f
Yuriy Golikov <meetmatt@ya.ru>
2014-07-30 03:18:27 +0400

    Moved application initialization to bootstrap file

---
+++ config/application.config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'modules' => [
+
+    ],
+    'module_listener_options' => [
+        'module_paths' => [
+            MODULE_DIR,
+            VENDOR_DIR
+        ],
+
+        /**
+         * Set configuration files autoloader glob pattern
+         *
+         * Loading priority:
+         *
+         *  1. config/global.php
+         *     config/local.php
+         *  2. config/MODULE_A/global.php
+         *     config/MODULE_A/local.php
+         *  3. config/MODULE_B/global.php
+         *     config/MODULE_B/local.php
+         */
+        'config_glob_paths' => [
+            CONFIG_DIR . '/{,*/}{global,local}.php'
+        ]
+    ]
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../bootstrap.php';
+
+Zend\Mvc\Application::init(require __DIR__ . '/../config/application.config.php')->run();
